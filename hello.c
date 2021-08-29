96f2211383f57c7e9dd34a5bae203a317938e960
Nathaniel Robinson <nathanielrobinson430@gmail.com>
2021-08-29 23:33:51 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Nathaniel Robinson
+ *         nrobinson8@unl.edu
+ * Date: 2021/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Nathaniel Robinson\n");
+
+  return 0;
+}
